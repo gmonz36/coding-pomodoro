22b63dace3e387c938296a72808602fd0f4f3c70
guillaumem <Guillaume_Monzerol@trendmicro.com>
2022-08-11 11:12:55 -0400

    Add prettier formatter

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,46 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import PomodoroManager from "./pomodoro";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const config = vscode.workspace.getConfiguration("pomodoro");
-	const pomodoroManager = new PomodoroManager(config.workTime, config.pauseTime);
+  const config = vscode.workspace.getConfiguration("pomodoro");
+  const pomodoroManager = new PomodoroManager(
+    config.workTime,
+    config.pauseTime
+  );
 
-	// list of commands
-	const startDisposable = vscode.commands.registerCommand("extension.startPomodoro", () => {
-		pomodoroManager.start();
-	});
+  // list of commands
+  const startDisposable = vscode.commands.registerCommand(
+    "extension.startPomodoro",
+    () => {
+      pomodoroManager.start();
+    }
+  );
 
-	const stopDisposable = vscode.commands.registerCommand("extension.pausePomodoro", () => {
-		pomodoroManager.pause();
-	});
+  const stopDisposable = vscode.commands.registerCommand(
+    "extension.pausePomodoro",
+    () => {
+      pomodoroManager.pause();
+    }
+  );
 
-	const resetDisposable = vscode.commands.registerCommand("extension.resetPomodoro", () => {
-		pomodoroManager.reset();
-	});
+  const resetDisposable = vscode.commands.registerCommand(
+    "extension.resetPomodoro",
+    () => {
+      pomodoroManager.reset();
+    }
+  );
 
-	// Add to a list of disposables which are disposed when this extension is deactivated.
-	context.subscriptions.push(pomodoroManager, startDisposable, stopDisposable, resetDisposable);
+  // Add to a list of disposables which are disposed when this extension is deactivated.
+  context.subscriptions.push(
+    pomodoroManager,
+    startDisposable,
+    stopDisposable,
+    resetDisposable
+  );
 }
 
 // this method is called when your extension is deactivated
--- src/pomodoro.ts
+++ src/pomodoro.ts
@@ -1,313 +1,329 @@
 import { StatusBarAlignment, StatusBarItem, window } from "vscode";
 
 enum PomodoroStatus {
-	None,
-	Work,
-	Rest,
-	Paused,
-	Break,
-	Done
+  None,
+  Work,
+  Rest,
+  Paused,
+  Break,
+  Done,
 }
 
 const timer = {
-    pomodoro: 25,
-    shortBreak: 5,
-    longBreak: 15,
-    longBreakInterval: 4,
-    second: 1000
-  }
+  pomodoro: 25,
+  shortBreak: 5,
+  longBreak: 15,
+  longBreakInterval: 4,
+  second: 1000,
+};
 
 class Timer {
-	private _timerId: any;
-	public get isRunning() {
-		return this._timerId !== null;
-	}
+  private _timerId: any;
+  public get isRunning() {
+    return this._timerId !== null;
+  }
 
-	constructor(public currentTime: number = 0, public interval: number = 1000) {
-        this._timerId = null;
+  constructor(public currentTime: number = 0, public interval: number = 1000) {
+    this._timerId = null;
+  }
+
+  public reset(time: number) {
+    this.stop();
+    this.currentTime = time;
+  }
+
+  public start(callback: { (): void; (): void }) {
+    if (this._timerId === null) {
+      this._timerId = setInterval(() => {
+        this.tick();
+        callback();
+      }, this.interval);
+    } else {
+      console.error("A timer instance is already running...");
+    }
+  }
+
+  public stop() {
+    if (this._timerId != null) {
+      clearInterval(this._timerId);
     }
 
-	public reset(time: number) {
-		this.stop();
-		this.currentTime = time;
-	}
+    this._timerId = null;
+  }
 
-	public start(callback: { (): void; (): void; }) {
-		if (this._timerId === null) {
-			this._timerId = setInterval(() => {
-				this.tick();
-				callback();
-			}, this.interval);
-		}
-		else {
-			console.error("A timer instance is already running...");
-		}
-	}
-
-	public stop() {
-		if (this._timerId != null) {
-			clearInterval(this._timerId);
-		}
-
-		this._timerId = null;
-	}
-
-	private tick() {
-		this.currentTime -= this.interval / 1000;
-	}
+  private tick() {
+    this.currentTime -= this.interval / 1000;
+  }
 }
 
 class Pomodoro {
-	// properties
-	private _status: PomodoroStatus = PomodoroStatus.None;
+  // properties
+  private _status: PomodoroStatus = PomodoroStatus.None;
 
-	public get status() {
-		return this._status;
-	}
-	public set status(status: PomodoroStatus) {
-		this._status = status;
-	}
+  public get status() {
+    return this._status;
+  }
+  public set status(status: PomodoroStatus) {
+    this._status = status;
+  }
 
-	private _timer: Timer;
+  private _timer: Timer;
 
-	public get timer() {
-		return this._timer;
-	}
+  public get timer() {
+    return this._timer;
+  }
 
-	// events
-	public onTick: (() => void) | undefined;
+  // events
+  public onTick: (() => void) | undefined;
 
-	constructor(public workTime: number = 25 * 60, public pauseTime: number = 5 * 60) {
-		this.workTime = Math.floor(this.workTime);
-		this.pauseTime = Math.floor(this.pauseTime);
+  constructor(
+    public workTime: number = 25 * 60,
+    public pauseTime: number = 5 * 60
+  ) {
+    this.workTime = Math.floor(this.workTime);
+    this.pauseTime = Math.floor(this.pauseTime);
 
-		this._timer = new Timer();
-		this.status = PomodoroStatus.None;
-	}
+    this._timer = new Timer();
+    this.status = PomodoroStatus.None;
+  }
 
-	// private methods
-	private done() {
-		this.stop();
-		this.status = PomodoroStatus.Done;
-	}
+  // private methods
+  private done() {
+    this.stop();
+    this.status = PomodoroStatus.Done;
+  }
 
-	private resetTimer(status: PomodoroStatus) {
-		if (status === PomodoroStatus.Work) {
-			this.timer.reset(this.workTime);
-		}
-		if (status === PomodoroStatus.Rest) {
-			this.timer.reset(this.pauseTime);
-		}
-	}
+  private resetTimer(status: PomodoroStatus) {
+    if (status === PomodoroStatus.Work) {
+      this.timer.reset(this.workTime);
+    }
+    if (status === PomodoroStatus.Rest) {
+      this.timer.reset(this.pauseTime);
+    }
+  }
 
-	// public methods
-	public start(status: PomodoroStatus = PomodoroStatus.Work) {
-		if (status === PomodoroStatus.Work || status === PomodoroStatus.Rest) {
-			if (this.status !== PomodoroStatus.Paused) {
-				this.resetTimer(status);
-			}
+  // public methods
+  public start(status: PomodoroStatus = PomodoroStatus.Work) {
+    if (status === PomodoroStatus.Work || status === PomodoroStatus.Rest) {
+      if (this.status !== PomodoroStatus.Paused) {
+        this.resetTimer(status);
+      }
 
-			this.status = status;
+      this.status = status;
 
-			this._timer.start(() => {
-				// stop the timer if no second left
-				if (this.timer.currentTime <= 0) {
-					if (this.status === PomodoroStatus.Work) {
-						window.showInformationMessage("Work done! Take a break.");
-						this.start(PomodoroStatus.Rest);
-					}
-					else if (this.status === PomodoroStatus.Rest) {
-						window.showInformationMessage("Pause is over.");
-						this.done();
-					}
-				}
+      this._timer.start(() => {
+        // stop the timer if no second left
+        if (this.timer.currentTime <= 0) {
+          if (this.status === PomodoroStatus.Work) {
+            window.showInformationMessage("Work done! Take a break.");
+            this.start(PomodoroStatus.Rest);
+          } else if (this.status === PomodoroStatus.Rest) {
+            window.showInformationMessage("Pause is over.");
+            this.done();
+          }
+        }
 
-				if (this.onTick) {
-					this.onTick();
-				}
-			});
-		}
-		else {
-			console.error("Start timer error");
-		}
-	}
+        if (this.onTick) {
+          this.onTick();
+        }
+      });
+    } else {
+      console.error("Start timer error");
+    }
+  }
 
-	public pause() {
-		this.stop();
-		this.status = PomodoroStatus.Paused;
-	}
+  public pause() {
+    this.stop();
+    this.status = PomodoroStatus.Paused;
+  }
 
-	public reset() {
-		this.stop();
-		this.status = PomodoroStatus.None;
-		this._timer.currentTime = this.workTime;
-	}
+  public reset() {
+    this.stop();
+    this.status = PomodoroStatus.None;
+    this._timer.currentTime = this.workTime;
+  }
 
-	public stop() {
-		this._timer.stop();
-	}
+  public stop() {
+    this._timer.stop();
+  }
 
-	public dispose() {
-		this.stop();
-		this.status = PomodoroStatus.None;
-	}
+  public dispose() {
+    this.stop();
+    this.status = PomodoroStatus.None;
+  }
 }
 
 class PomodoroManager {
-	// logic properties
-	private _pomodoroIndex: number = 0;
-	public pomodori: Pomodoro[] = [];
+  // logic properties
+  private _pomodoroIndex: number = 0;
+  public pomodori: Pomodoro[] = [];
 
-	public get currentPomodoro() {
-		return this.pomodori[this._pomodoroIndex];
-	}
+  public get currentPomodoro() {
+    return this.pomodori[this._pomodoroIndex];
+  }
 
-	public get currentState() {
-		switch (this.currentPomodoro.status) {
-			case PomodoroStatus.Work:
-				return " - work";
-			case PomodoroStatus.Rest:
-				return " - rest";
-			case PomodoroStatus.Paused:
-				return " - paused";
-			case PomodoroStatus.Break:
-					return " - break";
-			default:
-				return "";
-		}
-	}
+  public get currentState() {
+    switch (this.currentPomodoro.status) {
+      case PomodoroStatus.Work:
+        return " - work";
+      case PomodoroStatus.Rest:
+        return " - rest";
+      case PomodoroStatus.Paused:
+        return " - paused";
+      case PomodoroStatus.Break:
+        return " - break";
+      default:
+        return "";
+    }
+  }
 
-	public get isSessionFinished(): boolean {
-		return !this.currentPomodoro;
-	}
+  public get isSessionFinished(): boolean {
+    return !this.currentPomodoro;
+  }
 
-	// UI properties
-	private _statusBarText: StatusBarItem;
-	private _statusBarStartButton: StatusBarItem;
-	private _statusBarPauseButton: StatusBarItem;
-    private _statusBarResetButton: StatusBarItem;
+  // UI properties
+  private _statusBarText: StatusBarItem;
+  private _statusBarStartButton: StatusBarItem;
+  private _statusBarPauseButton: StatusBarItem;
+  private _statusBarResetButton: StatusBarItem;
 
-	constructor(public workTime: number = 25, public pauseTime: number = 5) {
-		// create status bar items
-			this._statusBarText = window.createStatusBarItem(StatusBarAlignment.Left);
-			this._statusBarText.show();
+  constructor(public workTime: number = 25, public pauseTime: number = 5) {
+    // create status bar items
+    this._statusBarText = window.createStatusBarItem(StatusBarAlignment.Left);
+    this._statusBarText.show();
 
+    this._statusBarStartButton = window.createStatusBarItem(
+      StatusBarAlignment.Left
+    );
+    this._statusBarStartButton.text = "$(triangle-right)";
+    this._statusBarStartButton.command = "extension.startPomodoro";
+    this._statusBarStartButton.tooltip = "Start Pomodoro";
 
-			this._statusBarStartButton = window.createStatusBarItem(StatusBarAlignment.Left);
-			this._statusBarStartButton.text = "$(triangle-right)";
-			this._statusBarStartButton.command = "extension.startPomodoro";
-			this._statusBarStartButton.tooltip = "Start Pomodoro";
+    this._statusBarPauseButton = window.createStatusBarItem(
+      StatusBarAlignment.Left
+    );
+    this._statusBarPauseButton.text = "$(primitive-square)";
+    this._statusBarPauseButton.command = "extension.pausePomodoro";
+    this._statusBarPauseButton.tooltip = "Pause Pomodoro";
 
+    this._statusBarResetButton = window.createStatusBarItem(
+      StatusBarAlignment.Left
+    );
+    this._statusBarResetButton.text = "$(debug-restart)";
+    this._statusBarResetButton.command = "extension.resetPomodoro";
+    this._statusBarResetButton.tooltip = "Reset Pomodoro";
 
-			this._statusBarPauseButton = window.createStatusBarItem(StatusBarAlignment.Left);
-			this._statusBarPauseButton.text = "$(primitive-square)";
-			this._statusBarPauseButton.command = "extension.pausePomodoro";
-			this._statusBarPauseButton.tooltip = "Pause Pomodoro";
+    this.reset();
+    this.draw();
+  }
 
-			this._statusBarResetButton = window.createStatusBarItem(StatusBarAlignment.Left);
-			this._statusBarResetButton.text = "$(debug-restart)";
-			this._statusBarResetButton.command = "extension.resetPomodoro";
-			this._statusBarResetButton.tooltip = "Reset Pomodoro";
+  // private methods
+  private update() {
+    // handle launch of the next Pomodoro
+    if (this.currentPomodoro.status === PomodoroStatus.Done) {
+      this._pomodoroIndex++;
 
-		this.reset();
-		this.draw();
-	}
+      if (!this.isSessionFinished) {
+        this.start();
+      }
+    }
+  }
 
-	// private methods
-	private update() {
-		// handle launch of the next Pomodoro
-		if (this.currentPomodoro.status === PomodoroStatus.Done) {
-			this._pomodoroIndex++;
+  private draw() {
+    if (this.isSessionFinished) {
+      // show text when all Pomodoro sessions are over
+      this._statusBarText.text = "Restart session?";
+      this._statusBarStartButton.show();
+      this._statusBarPauseButton.hide();
 
-			if (!this.isSessionFinished) {
-				this.start();
-			}
-		}
-	}
+      // show message if user needs a longer break
+      if (this.pomodori.length > 1) {
+        window.showInformationMessage(
+          "Well done! You should now take a longer break."
+        );
+      }
 
-	private draw() {
-		if (this.isSessionFinished) {
-			// show text when all Pomodoro sessions are over
-			this._statusBarText.text = "Restart session?";
-			this._statusBarStartButton.show();
-			this._statusBarPauseButton.hide();
+      return;
+    }
 
-			// show message if user needs a longer break
-			if (this.pomodori.length > 1) {
-				window.showInformationMessage("Well done! You should now take a longer break.");
-			}
+    const seconds = this.currentPomodoro.timer.currentTime % 60;
+    const minutes = (this.currentPomodoro.timer.currentTime - seconds) / 60;
 
-			return;
-		}
+    // update status bar (text)
+    const timerPart =
+      (minutes < 10 ? "0" : "") +
+      minutes +
+      ":" +
+      (seconds < 10 ? "0" : "") +
+      seconds;
 
-		const seconds = this.currentPomodoro.timer.currentTime % 60;
-		const minutes = (this.currentPomodoro.timer.currentTime - seconds) / 60;
+    let pomodoroNumberPart = "";
+    if (this.pomodori.length > 1) {
+      pomodoroNumberPart +=
+        " (" +
+        (this._pomodoroIndex + 1) +
+        " out of " +
+        this.pomodori.length +
+        " pomodori)";
+    }
 
-		// update status bar (text)
-		const timerPart = ((minutes < 10) ? "0" : "") + minutes + ":" + ((seconds < 10) ? "0" : "") + seconds;
+    this._statusBarText.text =
+      timerPart + this.currentState + pomodoroNumberPart;
 
-		let pomodoroNumberPart = "";
-		if (this.pomodori.length > 1) {
-			pomodoroNumberPart += " (" + (this._pomodoroIndex + 1) + " out of " + this.pomodori.length + " pomodori)";
-		}
+    if (this.currentPomodoro.status === PomodoroStatus.None) {
+      this._statusBarStartButton.show();
+      this._statusBarPauseButton.hide();
+      this._statusBarResetButton.hide();
+    } else if (this.currentPomodoro.status === PomodoroStatus.Paused) {
+      this._statusBarStartButton.show();
+      this._statusBarPauseButton.hide();
+      this._statusBarResetButton.show();
+    } else {
+      this._statusBarStartButton.hide();
+      this._statusBarPauseButton.show();
+    }
 
-		this._statusBarText.text = timerPart + this.currentState + pomodoroNumberPart;
+    this._statusBarText.show();
+  }
 
-		if (this.currentPomodoro.status === PomodoroStatus.None) {
-			this._statusBarStartButton.show();
-			this._statusBarPauseButton.hide();
-            this._statusBarResetButton.hide();
-		} else if (this.currentPomodoro.status === PomodoroStatus.Paused) {
-			this._statusBarStartButton.show();
-			this._statusBarPauseButton.hide();
-            this._statusBarResetButton.show();
-        }
-		else {
-			this._statusBarStartButton.hide();
-			this._statusBarPauseButton.show();
-		}
+  // public methods
+  public start() {
+    // launch a new session if the previous is already finished
+    if (this.isSessionFinished) {
+      this._pomodoroIndex = 0;
+    }
 
-		this._statusBarText.show();
-	}
+    this.currentPomodoro.start();
+    this.currentPomodoro.onTick = () => {
+      this.update();
+      this.draw();
+    };
+  }
 
-	// public methods
-	public start() {
-		// launch a new session if the previous is already finished
-		if (this.isSessionFinished) {
-			this._pomodoroIndex = 0;
-		}
+  public pause() {
+    this.currentPomodoro.pause();
 
-		this.currentPomodoro.start();
-		this.currentPomodoro.onTick = () => {
-			this.update();
-			this.draw();
-		};
-	}
+    this.update();
+    this.draw();
+  }
 
-	public pause() {
-		this.currentPomodoro.pause();
+  public reset() {
+    this._pomodoroIndex = 0;
+    this.pomodori = [];
 
-		this.update();
-		this.draw();
-	}
+    this.pomodori.push(new Pomodoro(this.workTime * 60, this.pauseTime * 60));
+  }
 
-	public reset() {
-		this._pomodoroIndex = 0;
-		this.pomodori = [];
+  public dispose() {
+    // stop current Pomodoro
+    this.currentPomodoro.dispose();
 
-		this.pomodori.push(new Pomodoro(this.workTime * 60, this.pauseTime * 60));
-	}
-
-	public dispose() {
-		// stop current Pomodoro
-		this.currentPomodoro.dispose();
-
-		// reset UI
-		this._statusBarText.dispose();
-		this._statusBarStartButton.dispose();
-		this._statusBarPauseButton.dispose();
-        this._statusBarResetButton.dispose();
-	}
+    // reset UI
+    this._statusBarText.dispose();
+    this._statusBarStartButton.dispose();
+    this._statusBarPauseButton.dispose();
+    this._statusBarResetButton.dispose();
+  }
 }
 
 export default PomodoroManager;
